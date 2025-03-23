e32869c612ee9fff2013024ccedcc01a05cb6996
venzy <david.venz@gmail.com>
2025-03-23 21:27:36 +1000

    Ch4 Following L1 Follow

--- main.go
+++ main.go
@@ -57,6 +57,8 @@ func main() {
 	cliCommands.register("agg", handlerAgg)
 	cliCommands.register("addfeed", handlerAddFeed)
 	cliCommands.register("feeds", handlerFeeds)
+	cliCommands.register("follow", handlerFollow)
+	cliCommands.register("following", handlerFollowing)
 
 	// Get command line args
 	if len(os.Args) < 2 {
@@ -128,7 +130,14 @@ func handlerRegister(s *state, cmd command) error {
 	}
 
 	now := time.Now()
-	user, err = s.db.CreateUser(context.Background(), database.CreateUserParams{ID: uuid.New(), CreatedAt: now, UpdatedAt: now, Name: username})
+	user, err = s.db.CreateUser(
+		context.Background(),
+		database.CreateUserParams{
+			ID: uuid.New(),
+			CreatedAt: now,
+			UpdatedAt: now,
+			Name: username,
+		})
 	if err != nil {
 		return err
 	}
@@ -244,7 +253,22 @@ func handlerAddFeed(s *state, cmd command) error {
 		return fmt.Errorf("Problem creating feed: %v", err)
 	}
 
-	fmt.Printf("New Feed: %v", newFeed)
+	// Auto-create new feed_follows entry
+	_, err = s.db.CreateFeedFollow(
+		context.Background(),
+		database.CreateFeedFollowParams{
+			ID: uuid.New(),
+			CreatedAt: now,
+			UpdatedAt: now,
+			UserID: user.ID,
+			FeedID: newFeed.ID,
+		})
+	
+	if err != nil {
+		return fmt.Errorf("Problem creating new follow: %v", err)
+	}
+
+	fmt.Printf("New Feed: %v\n", newFeed)
 
 	return nil
 }
@@ -262,6 +286,65 @@ func handlerFeeds(s *state, _ command) error {
 		}
 
 		fmt.Printf("%s %s %s\n", feedData.Name, feedData.Url, user.Name)
+	}
+
+	return nil
+}
+
+func handlerFollow(s *state, cmd command) error {
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("follow requires one argument, the feed URL")
+	}
+
+	feedURL := cmd.args[0]
+
+	// Get logged in user ID
+	user, err := s.db.GetUserByName(context.Background(), s.cfg.CurrentUserName)
+	if err != nil {
+		return fmt.Errorf("Current user '%s' not in database!", s.cfg.CurrentUserName)
+	}
+
+	// Get feed ID from URL
+	feed, err := s.db.GetFeedByURL(context.Background(), feedURL)
+	if err != nil {
+		return fmt.Errorf("Feed URL '%s' not in database!", feedURL)
+	}
+
+	now := time.Now()
+	newFeedFollow, err := s.db.CreateFeedFollow(
+		context.Background(),
+		database.CreateFeedFollowParams{
+			ID: uuid.New(),
+			CreatedAt: now,
+			UpdatedAt: now,
+			UserID: user.ID,
+			FeedID: feed.ID,
+		})
+
+	if err != nil {
+		return fmt.Errorf("Problem creating feed_follows record for user '%s' and feed URL '%s'", user.Name, feed.Url)
+	}
+
+	fmt.Printf("User '%s' now following feed '%s'\n", newFeedFollow.UserName, newFeedFollow.FeedName)
+
+	return nil
+}
+
+func handlerFollowing(s *state, _ command) error {
+	// Get user ID
+	user, err := s.db.GetUserByName(context.Background(), s.cfg.CurrentUserName)
+	if err != nil {
+		return fmt.Errorf("User '%s' not in database!", s.cfg.CurrentUserName)
+	}
+
+	// Get follows
+	follows, err := s.db.GetFeedFollowsForUser(context.Background(), user.ID)
+	if err != nil {
+		return fmt.Errorf("Problem fetching follows for user '%s': %v", s.cfg.CurrentUserName, err)
+	}
+
+	for _, follow := range follows {
+		fmt.Printf("%s\n", follow.FeedName)
 	}
 
 	return nil
